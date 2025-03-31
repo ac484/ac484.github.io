4fca539e994e6ab6e5b22d73f7f0b46dbfca677f
𓂺 <ac7x7x@gmail.com>
2025-03-31 11:51:19 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export', // 靜態導出
   images: {
     unoptimized: true, // 禁用圖片優化，因為 GitHub Pages 不支援圖片優化服務
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
