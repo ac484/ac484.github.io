2cacdf0a16ba30a4337ff44c640d313df9966e19
𓂺 <ac7x7x@gmail.com>
2025-03-31 11:17:48 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // 靜態導出
   images: {
-    unoptimized: true, // 禁用圖片優化
+    unoptimized: true, // 禁用圖片優化，因為 GitHub Pages 不支援圖片優化服務
   },
 };
 
