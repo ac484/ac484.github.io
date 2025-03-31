54d60b6cb1928919a372a118858d89b7e7607586
𓂺 <ac7x7x@gmail.com>
2025-03-31 11:56:24 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // 靜態導出
+    images: {
+      unoptimized: true, // 禁用圖片優化，因為 GitHub Pages 不支援圖片優化服務
+    },
+  };
+  
+  module.exports = nextConfig;
